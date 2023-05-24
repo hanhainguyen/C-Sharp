8763adada14891776e03175ff4ac9e1ac3d8e6f3
Nguyen Han <134388150+hanhainguyen@users.noreply.github.com>
2023-05-24 12:29:45 +0700

    ConsoleApp code

---
+++ ConsoleApp/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Nhap so nguyen thu 1");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so nguyen thu 2");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so nguyen thu 3");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("|So nguyen 1|So nguyen 2|So nguyen3");
+            Console.Writeline("| {0} | {1} | {2}  |", a, b, c);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sx;
+            Console.WriteLine("Moi ban nhap vao 3 so nguyen:");
+            Console.WriteLine("Nhap so nguyen thu 1");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so nguyen thu 2");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Nhap so nguyen thu 3");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("|So nguyen 1|So nguyen 2|So nguyen3");
+            Console.WriteLine("| {0} | {1} | {2} | ", a, b, c);
+            Console.WriteLine("Day so sau khi sap xep la :");
+            if (a > b)
+            {
+                sx = a;
+                a = b;
+                b = sx;
+            }
+            if (a > c)
+            {
+                sx = a;
+                a = c;
+                c = sx;
+            }
+            if (b > c)
+            {
+                sx = b;
+                b = c;
+                c = sx;
+            }
+            Console.Write("| {0}  |  {1}  |  {2}  | ", a, b, c);
+        }
+    }
+}
